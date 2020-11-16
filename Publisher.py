9c809923f2df7d7566533172bf91d6b2a5183642
Anissarfaoui <50216993+Anissarfaoui@users.noreply.github.com>
2020-11-16 10:07:15 +0100

    Update Publisher.py

--- Publisher.py
+++ Publisher.py
@@ -24,7 +24,8 @@ while True:
             publisher = publisher_links[i]
             publisher_data = {
                 "Name_Publisher": publisher.find_element_by_css_selector('mat-card-title.mat-card-title').text,
-                "Description": publisher.find_element_by_css_selector('mat-card-subtitle.mat-card-subtitle').text}
+                "Link_name": publisher.find_element_by_css_selector('mat-card-subtitle.mat-card-subtitle > h2').text,
+                "Description": publisher.find_element_by_css_selector('mat-card-subtitle.mat-card-subtitle > p').text}
 
             try:
                 publisher_data["Collections"] = publisher.find_element_by_css_selector(
